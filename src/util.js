d8c9a80bdd314a837230fe1d31b28197f7656f53
tdubs <thomas.wu@kp.org>
2016-09-16 18:17:31 -0700

    adding a utility fily for utility type of functions

---
+++ src/util.js
@@ -0,0 +1,18 @@
+let ready = (fn) => {
+  if (document.readyState != 'loading') {
+    fn();
+  } else if (document.addEventListener) {
+    document.addEventListener('DOMContentLoaded', fn);
+  } else {
+    document.attachEvent('onreadystatechange', function() {
+      if (document.readyState != 'loading')
+        fn();
+    });
+  }
+  if(document.readyState === "complete") {
+    return fn();
+  }
+}
+
+
+export { ready };
