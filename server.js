273a835c16a13692e41ff26b0fe03d7b3d545459
tdubs <thomas.wu@kp.org>
2016-09-17 17:51:54 -0700

    adding spaces

--- server.js
+++ server.js
@@ -3,11 +3,9 @@ const WebpackDevServer = require('webpack-dev-server');
 const config = require('./webpack.config');
 
 new WebpackDevServer(webpack(config), {
-
   path: config.output.path,
   publicPath: config.output.publicPath,
   historyApiFallback: true
-
 }).listen(3000, 'localhost', (err) => {
   if (err) { console.error(err); }
   console.log('Listening at http://localhost:3000/');
