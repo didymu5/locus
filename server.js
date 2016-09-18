f253ae9a7d0ecd2c5e7274fe51e8ccf566df2f32
tdubs <thomas.wu@kp.org>
2016-09-17 17:51:11 -0700

    removing hot reload

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const WebpackDevServer = require('webpack-dev-server');
 const config = require('./webpack.config');
 
 new WebpackDevServer(webpack(config), {
-  path: 'public',
+
+  path: config.output.path,
   publicPath: config.output.publicPath,
-  hot: true,
   historyApiFallback: true
+
 }).listen(3000, 'localhost', (err) => {
   if (err) { console.error(err); }
   console.log('Listening at http://localhost:3000/');
