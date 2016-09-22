c43142e208aa07670a2b2262ed6a15f7937560fc
tdubs <thomas.wu@kp.org>
2016-09-21 17:16:01 -0700

    http stateless in the url

--- src/main.js
+++ src/main.js
@@ -12,7 +12,20 @@ ready(() => {
     $longtitude_txt = document.getElementById('longitude-txt'),
     $timestamp_txt = document.getElementById('timestamp-txt'),
     $loading_indicator = document.getElementById('loading-indicator'),
-    $coordinates_block = document.getElementById('coordinates-block');
+    $coordinates_block = document.getElementById('coordinates-block'),
+    $button__mark_this = document.getElementById('button--mark-this');
+
+    toggleLoader('show', $loading_indicator, $coordinates_block);
+
+  let locationParams = getAllParams().loc;
+  let coord_regex =  /^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/;
+
+  if(locationParams != null && coord_regex.test(locationParams)) {
+    $latitude_txt.innerHTML = locationParams.split(',')[0];
+    $longtitude_txt.innerHTML = locationParams.split(',')[1];
+    toggleLoader('hide', $loading_indicator, $coordinates_block);
+    return false;
+  }
 
   let options = {
     enableHighAccuracy: true,
@@ -22,12 +35,12 @@ ready(() => {
 
   let locator = new getCoordinates(options);
 
-  $coordinates_block.style.display = 'none';
-  $loading_indicator.style.display = 'block';
-
   locator.then((res) => {
-    $loading_indicator.style.display = 'none';
-    $coordinates_block.style.display = 'block';
+    toggleLoader('hide', $loading_indicator, $coordinates_block);
+
+    let query_loc_string =  `?loc=${res.coords.latitude},${res.coords.longitude}`;
+
+    window.history.pushState({loc: res.coords.latitude+','+res.coords.longitude}, res.coords.latitude+','+res.coords.longitude, query_loc_string)
     $latitude_txt.innerHTML = res.coords.latitude;
     $longtitude_txt.innerHTML = res.coords.longitude;
     $timestamp_txt.innerHTML = moment(res.timestamp);
